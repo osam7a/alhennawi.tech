07614740c57cdeaabb2e88ed947fdc151d84607e
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-10-24 16:33:52 +0400

    Slight changes to passenger wsgi for compatibility issues

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,7 @@
-import os
-import sys
+import sys, os
+
+INTERP = os.environ.get('PYTHON_INTERPRETER', 0)
+if INTERP == 0: sys.stderr.write("No PYTHON_INTERPRETER found\n"); sys.exit(1)
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from alhennawitech.wsgi import application
