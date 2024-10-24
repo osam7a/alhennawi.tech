9aea86d0370ca357fef10356e37d685500506d0a
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-10-24 15:58:34 +0400

    set default port to 30001, and add my domain to allowed hosts

--- alhennawitech/settings.py
+++ alhennawitech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = environ['DJANGO_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.143", "localhost"]
+ALLOWED_HOSTS = ["192.168.0.143", "localhost", "alhennawi.tech"]
 
 
 # Application definition
--- manage.py
+++ manage.py
@@ -6,6 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "30001"
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alhennawitech.settings')
     try:
         from django.core.management import execute_from_command_line
